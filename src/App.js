e472de1f469c5e49af10cf35fa1c303f27775532
Manan Joshi <mananjoshi1995@gmail.com>
2020-09-12 01:57:02 -0500

    Getting tailwind working and adding a sample input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function App() {
-  return <div className='text-teal-500'>Hello Friend</div>;
+  return (
+    <div className='grid place-items-center h-full'>
+      <label htmlFor='word' className='w-2/4 text-center'>
+        {/* Word Search */}
+        <input
+          id='word'
+          className='bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal w-full'
+          type='text'
+          placeholder='Please enter a word to search'
+        />
+      </label>
+    </div>
+  );
 }
 
 export default App;
